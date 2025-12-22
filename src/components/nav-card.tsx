ec4695773b1376f6a66b53d9d87e342cf4f8f828
zhaqiang123345667899 <122604336+zhaqiang123345667899@users.noreply.github.com>
2025-12-22 17:26:59 +0800

    Update nav-card.tsx

--- src/components/nav-card.tsx
+++ src/components/nav-card.tsx
@@ -28,14 +28,8 @@ const list = [
 	{
 		icon: ScrollOutlineSVG,
 		iconActive: ScrollFilledSVG,
-		label: '近期文章',
+		label: '我的笔记',
 		href: '/blog'
-	},
-	{
-		icon: ProjectsOutlineSVG,
-		iconActive: ProjectsFilledSVG,
-		label: '我的项目',
-		href: '/projects'
 	},
 	{
 		icon: AboutOutlineSVG,
@@ -49,12 +43,6 @@ const list = [
 		label: '推荐分享',
 		href: '/share'
 	},
-	{
-		icon: WebsiteOutlineSVG,
-		iconActive: WebsiteFilledSVG,
-		label: '优秀博客',
-		href: '/bloggers'
-	}
 ]
 
 const extraSize = 8
@@ -141,7 +129,7 @@ export default function NavCard() {
 					<Link className='flex items-center gap-3' href='/'>
 						<Image src='/images/avatar.png' alt='avatar' width={40} height={40} style={{ boxShadow: ' 0 12px 20px -5px #E2D9CE' }} className='rounded-full' />
 						{form === 'full' && <span className='font-averia mt-1 text-2xl leading-none font-medium'>{siteContent.meta.title}</span>}
-						{form === 'full' && <span className='text-brand mt-2 text-xs font-medium'>(开发中)</span>}
+						{form === 'full' && <span className='text-brand mt-2 text-xs font-medium'>(休眠中)</span>}
 					</Link>
 
 					{(form === 'full' || form === 'icons') && (
